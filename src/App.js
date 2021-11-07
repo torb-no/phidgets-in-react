e0c5678084732c537e4d0be5216b32b30df280b4
Torb Lunde Miranda <1179046+torb-xyz@users.noreply.github.com>
2021-11-07 18:31:25 +0100

    Basic phidgets example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
+// We need to disable no-undef eslint rule since
+// eslint doesn't know about phidgets
+
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [distance, setDistance] = useState(-1);
+
+  const ledRef = useRef();
+
+  useEffect(() => {
+    const phidgetConnection = new phidget22.Connection({
+      hostname: 'localhost',
+      port: 8989,
+    });
+
+    phidgetConnection.connect().then(() => {
+      console.log('Connected to Phidgets')
+
+      const distanceSensor = new phidget22.DistanceSensor();
+      distanceSensor.onDistanceChange = function(distance) {
+        setDistance(distance);
+      };
+
+      distanceSensor.open().catch(err => {
+        console.error('Error opening sensor: ' + err)
+      });
+
+      const led = new phidget22.DigitalOutput();
+      led.setHubPort(2);
+      led.setIsHubPortDevice(true);
+
+      led.open().then(() => {
+        ledRef.current = led;
+      }).catch(err => {
+        console.error('Error opening LED: ' + err);
+      });
+
+    }).catch(err => {
+      console.error('Error during connect: ' + err);
+    }) ;
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Distance: {distance}</p>
+
+      <button onClick={() => {
+        ledRef.current.setState(true);
+      }}>
+        On
+      </button>
+
+      <button onClick={() => {
+        ledRef.current.setState(false);
+      }}>
+        Off
+      </button>
     </div>
   );
 }
